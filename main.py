dc0eb20782d043a79f3ed1bafcfa90ca868841de
welder7818 <welder7818@gmail.com>
2025-05-12 18:03:02 -0300

    Render

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, redirect  # importe redirect
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -7,35 +7,36 @@ from app.routes import routes_blueprint
 from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app():
-    # Carrega .env
     load_dotenv()
+    swagger_url  = os.getenv('SWAGGER_URL', '/swagger')
+    api_docs     = os.getenv('SWAGGER_JSON_PATH', '/static/swagger.json')
 
-    # Cria app e configura pasta de estáticos
     app = Flask(
         __name__,
         static_folder='static',
         static_url_path='/static'
     )
-
-    # Habilita CORS apenas para /api/*
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     # JWT
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'troque_essa_chave!')
     JWTManager(app)
 
-    # Rotas da API
+    # API routes
     app.register_blueprint(routes_blueprint, url_prefix='/api')
 
     # Swagger UI
-    swagger_url  = os.getenv('SWAGGER_URL', '/swagger')
-    api_docs     = os.getenv('SWAGGER_JSON_PATH', '/static/swagger.json')
     swagger_bp = get_swaggerui_blueprint(
         swagger_url,
         api_docs,
         config={'app_name': "API Vitivinicultura Embrapa"}
     )
     app.register_blueprint(swagger_bp, url_prefix=swagger_url)
+
+    # **Rota raiz redirecionando para o Swagger UI**
+    @app.route('/')
+    def index():
+        return redirect(swagger_url)
 
     return app
 
